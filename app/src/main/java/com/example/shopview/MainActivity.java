5ed68dc4f1b005ff5a28cf3cea5815fa7e9998d6
fhkhd <fhkhdkri@yahoo.com>
2022-05-01 15:49:23 +0430

    click to home

--- app/src/main/java/com/example/shopview/MainActivity.java
+++ app/src/main/java/com/example/shopview/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.shopview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.shopview.activity.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enter = findViewById(R.id.enter_app);
+        enter.setOnClickListener(view -> {
+            Intent intent =new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/example/shopview/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.shopview.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shopview.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
