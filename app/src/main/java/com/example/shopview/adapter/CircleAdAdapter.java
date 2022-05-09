354c3acf6dd284adb87dc0f74a7a5a8e8de21bd6
fhkhd <fhkhdkri@yahoo.com>
2022-05-09 14:17:38 +0430

    ad circle adapter

--- app/src/main/java/com/example/shopview/adapter/CircleAdAdapter.java
+++ app/src/main/java/com/example/shopview/adapter/CircleAdAdapter.java
@@ -1,4 +1,45 @@
 package com.example.shopview.adapter;
 
-public class CircleAdAdapter{
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.shopview.R;
+
+public class CircleAdAdapter extends RecyclerView.Adapter<CircleAdAdapter.ViewHolder> {
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.ad_circle_item,parent,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        holder.circle.setColorFilter(R.color.teal_200);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView circle ;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            circle = itemView.findViewById(R.id.circle_ad_image);
+
+        }
+    }
 }
